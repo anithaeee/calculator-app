643e380e3adc33adb21d857a7f11d2cbf784939f
Nageshwar Reddy Pandem <nageshwar.uideveloper@gmail.com>
2023-11-30 13:42:20 +0800

    nov 30th

--- src/calculator-app/Calculator.js
+++ src/calculator-app/Calculator.js
@@ -11,21 +11,22 @@ const Calculator = () => {
     switch(val){
         case 'clear':
         let ques=question;
-        ques=ques.substring(0,ques,length-1);
+        let length = ques.length;
+        ques=ques.substring(0,length-1);
         setquestion(ques);
         break;
        case 'delete':
         setquestion(""); 
         setanswer("");
         break;
-        case '=':
+     case '=':
          let data = question;
          let result = eval(data);
           setanswer(result);
           break;
-          default:
+     default:
           let val1 = question;
-          setquestion(val1+data);
+          setquestion(val1+val);
             
     }
    }
@@ -38,32 +39,34 @@ const Calculator = () => {
         </div>
         <div className='card card2'>
             <div className='calinput' style={{marginLeft:'10px'}}>
-                <InputText val={question} style={{height:'36px',width:'80%',marginLeft:'10%',borderRadius:'10px'}}></InputText>
+                <InputText value={question} style={{height:'36px',width:'80%',marginLeft:'10%',borderRadius:'10px'}}></InputText>
+                <InputText value={answer} style={{height:'36px',width:'80%',marginLeft:'10%',borderRadius:'10px'}}></InputText>
+
         </div>
         {/* <div className='row'> */}
             <div className='btn'>
-            <Button className='button'  onclick={()=>{updatevalue('1')}}style={{backgroundColor:'lightgreen'}}>1</Button>
-            <Button className='button'  onclick={()=>{updatevalue('2')}}style={{backgroundColor:'lightgreen'}}>2</Button>
-            <Button className='button'  onclick={()=>{updatevalue('3')}}style={{backgroundColor:'lightgreen'}}>3</Button>
-            <Button className='button'  onclick={()=>{updatevalue('4')}}style={{backgroundColor:'lightgreen'}}>4</Button>
-            <Button className='button' onclick={()=>{updatevalue('5')}}style={{backgroundColor:'lightgreen'}}>5</Button>
-            <Button className='button' onclick={()=>{updatevalue('6')}}style={{backgroundColor:'lightgreen'}}>6</Button>
-            <Button className='button' onclick={()=>{updatevalue('7')}}style={{backgroundColor:'lightgreen'}}>7</Button>
-            <Button className='button' onclick={()=>{updatevalue('8')}}style={{backgroundColor:'lightgreen'}}>8</Button>
-            <Button className='button' onclick={()=>{updatevalue('9')}}style={{backgroundColor:'lightgreen'}}>9</Button>
-            <Button className='button' onclick={()=>{updatevalue('0')}}style={{backgroundColor:'lightgreen'}}>0</Button>
-            <Button className='button' onclick={()=>{updatevalue('clear')}}style={{backgroundColor:'lightgreen'}}>Clear</Button>
-            <Button className='button' onclick={()=>{updatevalue('delete')}}style={{backgroundColor:'lightgreen'}}>delete</Button>
-            <Button className='button' onclick={()=>{updatevalue('.')}}style={{backgroundColor:'lightgreen'}}>.</Button>
-             <Button className='button' onclick={()=>{updatevalue('=')}}style={{backgroundColor:'lightblue'}}>=</Button>
-             <Button  className='button' onclick={()=>{updatevalue('-')}}style={{backgroundColor:'lightblue'}}>-</Button>
-             <Button  className='button'onclick={()=>{updatevalue('*')}} style={{backgroundColor:'lightblue'}}>*</Button>
-             <Button  className='button' onclick={()=>{updatevalue('+')}}style={{backgroundColor:'lightblue'}}>÷</Button>
-             <Button  className='button' onclick={()=>{updatevalue('%')}}    
+            <Button className='button'  onClick={()=>{updatevalue('1')}}style={{backgroundColor:'lightgreen'}}>1</Button>
+            <Button className='button'  onClick={()=>{updatevalue('2')}}style={{backgroundColor:'lightgreen'}}>2</Button>
+            <Button className='button'  onClick={()=>{updatevalue('3')}}style={{backgroundColor:'lightgreen'}}>3</Button>
+            <Button className='button'  onClick={()=>{updatevalue('4')}}style={{backgroundColor:'lightgreen'}}>4</Button>
+            <Button className='button' onClick={()=>{updatevalue('5')}}style={{backgroundColor:'lightgreen'}}>5</Button>
+            <Button className='button' onClick={()=>{updatevalue('6')}}style={{backgroundColor:'lightgreen'}}>6</Button>
+            <Button className='button' onClick={()=>{updatevalue('7')}}style={{backgroundColor:'lightgreen'}}>7</Button>
+            <Button className='button' onClick={()=>{updatevalue('8')}}style={{backgroundColor:'lightgreen'}}>8</Button>
+            <Button className='button' onClick={()=>{updatevalue('9')}}style={{backgroundColor:'lightgreen'}}>9</Button>
+            <Button className='button' onClick={()=>{updatevalue('0')}}style={{backgroundColor:'lightgreen'}}>0</Button>
+            <Button className='button' onClick={()=>{updatevalue('clear')}}style={{backgroundColor:'lightgreen'}}>Clear</Button>
+            <Button className='button' onClick={()=>{updatevalue('delete')}}style={{backgroundColor:'lightgreen'}}>delete</Button>
+            <Button className='button' onClick={()=>{updatevalue('.')}}style={{backgroundColor:'lightgreen'}}>.</Button>
+             <Button className='button' onClick={()=>{updatevalue('=')}}style={{backgroundColor:'lightblue'}}>=</Button>
+             <Button  className='button' onClick={()=>{updatevalue('-')}}style={{backgroundColor:'lightblue'}}>-</Button>
+             <Button  className='button'onClick={()=>{updatevalue('*')}} style={{backgroundColor:'lightblue'}}>*</Button>
+             <Button  className='button' onClick={()=>{updatevalue('+')}}style={{backgroundColor:'lightblue'}}>÷</Button>
+             <Button  className='button' onClick={()=>{updatevalue('%')}}    
                 
         
                           style={{backgroundColor:'lightblue'}}>%</Button>
-             <Button  className='button' onclick={()=>{updatevalue('=')}}style={{backgroundColor:'lightblue'}}>=</Button>
+             <Button  className='button' onClick={()=>{updatevalue('=')}}style={{backgroundColor:'lightblue'}}>=</Button>
             </div>
         </div>
         
